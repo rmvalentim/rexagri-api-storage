e9193ee8585962df4443cafd88c4b89cf6c9d7e9
rmvalentim <rmoraesvalentim@gmail.com>
2022-09-16 14:32:38 -0300

    Removing autoincrement from Snapshots table

--- db/Snapshot.js
+++ db/Snapshot.js
@@ -4,8 +4,7 @@ import connection from './Connection.js'
 const Snapshot = connection.define('snapshot', {
 
     id: {
-        type: sequelize.INTEGER,
-        autoIncrement: true,
+        type: sequelize.INTEGER,        
         allowNull: false,
         primaryKey: true
     },
